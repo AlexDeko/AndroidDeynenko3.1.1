d5acec0d4560c74b279ccd3575eaa5a0e5b5bcff
Alexandr Deynenko <alex.deko.69@gmail.com>
2019-12-27 16:34:36 +0300

    Initial commit

---
+++ app/src/main/java/com/homework1_3/androiddeynenko311/MainActivity.java
@@ -0,0 +1,26 @@
+package com.homework1_3.androiddeynenko311;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        text = findViewById(R.id.text);
+    }
+}
